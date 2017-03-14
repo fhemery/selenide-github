ee09cc737d088d6d7e204b3e9061c856e49a1581
Hemery <frederic.hemery@capgemini.com>
2017-03-14 12:05:35 +0100

    Initial commit with project initialization

---
+++ src/test/java/fr/hemit/githubselenide/VeryBasicTest.java
@@ -0,0 +1,14 @@
+package fr.hemit.githubselenide;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VeryBasicTest {
+
+	@Test
+	public void test() {
+		assertEquals(5, 3+3);
+	}
+
+}
